17c37b9435be29005f7d0816a276e58560eda572
omkarketha <92929148+omkarketha@users.noreply.github.com>
2021-11-05 18:47:54 +0530

    Update SpringBootGitProjectSshApplication.java
    i am write hi

--- SpringBootGitProjectSSH/src/main/java/com/app/SpringBootGitProjectSshApplication.java
+++ SpringBootGitProjectSSH/src/main/java/com/app/SpringBootGitProjectSshApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootGitProjectSshApplication {
 
 	public static void main(String[] args) {
+		//Hi
 		SpringApplication.run(SpringBootGitProjectSshApplication.class, args);
 	}
 
