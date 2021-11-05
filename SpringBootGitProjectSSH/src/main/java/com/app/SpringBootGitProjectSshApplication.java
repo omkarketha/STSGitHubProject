8f6340f7edd263287912b62c2ba9052e0a940275
Siva <Siva@Siva-PC>
2021-11-05 18:42:45 +0530

    om nama sivaya

---
+++ SpringBootGitProjectSSH/src/main/java/com/app/SpringBootGitProjectSshApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitProjectSshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitProjectSshApplication.class, args);
+	}
+
+}
